eac21f13044bc6d6fb25563082d9ae79469cbc00
Manuel Bugallo <marvinody@gmail.com>
2019-05-13 10:38:08 -0400

    start of candy reducer

---
+++ app/reducers/candies.js
@@ -0,0 +1,11 @@
+
+const initialState = {}
+
+const candiesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default candiesReducer
