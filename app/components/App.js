565004b0d1c14893ca2db07eb3f3a6eda7347619
Manuel Bugallo <marvinody@gmail.com>
2019-05-13 12:04:21 -0400

    finished basic styling of the candies. Looks like shit tho

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-import Root from './/root';
+import { HashRouter as Router, NavLink, Route } from 'react-router-dom';
 import CandyList from './CandyList';
+import Root from './root';
 
 const App = () => (
   <Router>
     <div>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="candies">Candies</Link>
+        <NavLink activeClassName="selected" to="/">Home</NavLink>
+        <NavLink activeClassName="selected" to="candies">Candies</NavLink>
       </nav>
       <Route exact path="/" component={Root} />
       <Route exact path="/candies" component={CandyList} />
--- app/components/Candy.js
+++ app/components/Candy.js
@@ -5,10 +5,14 @@ const Candy = props => (
     <div className="candy image">
       <img src={props.imageUrl} />
     </div>
-    <div className="candy info">
-      <div className="candy title">{props.name}</div>
-      <div className="candy quantity">{props.quantity}</div>
-      <div className="candy desc">{props.description}</div>
+    <div className="candy all info">
+      <div className="candy basic info">
+        <div className="candy title">{props.name}</div>
+        <div className="candy quantity">{props.quantity}</div>
+      </div>
+      <div className="candy more info">
+        <div className="candy desc">{props.description}</div>
+      </div>
     </div>
   </div>
 )
