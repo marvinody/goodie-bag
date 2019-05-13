2f4a4121780aed27a352fb2d06f03f4dba1ba0e8
Manuel Bugallo <marvinody@gmail.com>
2019-05-13 10:20:24 -0400

    candy model done

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -3,4 +3,28 @@ const db = require('../database');
 
 module.exports = db.define('candy', {
   // define your model here!
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 1,
+      max: 100,
+    }
+  },
+  imageUrl: {
+    defaultValue: '/public/bg.jpg'
+  }
 });
